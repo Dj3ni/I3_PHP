f85a715c5981d38e27c67f61326458b21c03f716
Jen <fernandezgarcia.jenny@gmail.com>
2024-07-01 19:20:52 +0200

    repair links js

--- htdocs/Connexion DB/Exercices/DB ludothèque/nav.php
+++
@@ -1,8 +0,0 @@
-
-<nav>
-    <a href="index.php">Home</a>
-    <a href="LudoInsert.php">Ajouter un jeu</a>
-    <a href="LudoSearch.php">Chercher un jeu</a>
-    <a href="ajaxSearch.php">Recherche Ajax</a>
-    <a href="logout.php">Logout</a>
-</nav>
